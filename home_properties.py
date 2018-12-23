39868edd0656b034d2e5c145fdb4582d7ca8256a
Blowoffvalve <osunkoyaopeoluwa@gmail.com>
2018-12-22 18:46:02 -0600

    Added methods to get the reviews for a listing. Also changed the format of the output file and included an header

--- home_properties.py
+++ home_properties.py
@@ -4,22 +4,18 @@ class ListingSpider(scrapy.Spider):
     #Properties of the spider
     name = "ListingSpider"
     start_urls = ["https://www.airbnb.com/rooms/427077"]
-    
+    f= open("apartment.csv", "a+")
+    f.write("Listing_id, No_Of_Reviews, Listing_rating, *Amenities \n")
     #File handle to write the properties of a file to a CSV file
     #f = open("apartment.csv", "a+")
     
     def get_amenities(self, text):
         #File handle to write the properties of a file to a CSV file
-        f = open("apartment.csv", "a+")
         start_index = text.index('"listing_amenities":')
         stop_index = text.index("]", start_index)
         #substring the text to only include relevant data
         text = text[int(start_index): int(stop_index)]
         start_index = 0
-        
-        #Write the listing_Id to the file
-        url = str(self.start_urls)
-        f.write(url[url.index("/rooms")+7:-2])
         
         #Loop through the text to get the amenities
         #Get number of appearances of "name" in the text
@@ -27,15 +23,34 @@ class ListingSpider(scrapy.Spider):
         for x in range(name_count):
             start_index = text.index("name", start_index)+ 7
             stop_index = text.index('",', start_index)
-            f.write("," + text[start_index: stop_index])
-        f.write("\n")   
+            self.f.write(", " + text[start_index: stop_index])
+        self.f.write("\n")   
+    
+    def get_reviews(self, text):
+        start_index = text.index('"_1dl27thl">')+12
+        stop_index = text.index(" ", start_index)
+        self.f.write(", " + text[start_index: stop_index])
+        start_index = text.index('content="')+9
+        self.f.write(", " + text[start_index:start_index+1])
+        
     
     def parse(self, response):
+        #f = open("apartment.csv", "a+")
+        #Write the listing_Id to the file
+        url = str(self.start_urls)
+        self.f.write(url[url.index("/rooms")+7:-2])
         #Selector to get the script that contains "application/json"
-        SCRIPT_SELECTOR = "//script[@type ='application/json']/text()"
-        for script in response.xpath(SCRIPT_SELECTOR):
+        AMENITIES_SELECTOR = "//script[@type ='application/json']/text()"
+        RATING_SELECTOR = '//div[@id="reviews"]'
+        
+        #Get the reviews of the listing
+        self.get_reviews(str(response.xpath(RATING_SELECTOR).extract()))
+        
+        #Get the amenities of the listing
+        for script in response.xpath(AMENITIES_SELECTOR):
             text = script.extract()
             ## Get the listing_amenities
             if "listing_amenities" in text:
                 self.get_amenities(text)
-                
+        
+        
